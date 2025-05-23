44d458dde21037694118954d3a5314c57869b7f8
ARYAN MASSOUDY <116915753+aryan-massoudy@users.noreply.github.com>
2025-05-23 13:32:59 +0200

    debugged the settings, templates and views

--- simplesocial/posts/models.py
+++ simplesocial/posts/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.urls import reverse
 from django.db import models
 
-import misaka
+import misaka 
 
 from groups.models import Group
 
@@ -11,11 +11,11 @@ User = get_user_model()
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, related_name="posts")
+    user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
     message_html = models.TextField(editable=False)
-    group = models.ForeignKey(Group, related_name="posts",null=True, blank=True)
+    group = models.ForeignKey(Group, related_name="posts", null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.message
--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "accounts",
     "bootstrap3",
+    "accounts",
+    "groups",
+    "posts",
+    "django.contrib.humanize"
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -116,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIR = [os.path.join(BASE_DIR,"static")]
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
